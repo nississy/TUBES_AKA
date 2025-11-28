a4efb1293f58a6e0e05eb552f55e74d2333c951e
Nisa <theunknownbusiness1010@gmail.com>
2025-11-28 19:08:03 +0700

    Update main.cpp

--- TUBESAKA/main.cpp
+++ TUBESAKA/main.cpp
@@ -1,107 +1,152 @@
 #include <iostream>
 #include "peluang.h"
-
 using namespace std;
 
-// Variabel penghitung operasi
-long long operasiIteratif = 0;
-long long operasiRekursif = 0;
-
-// Faktorial iteratif dengan penghitung
-int faktorialIteratifCount(int n) {
-    operasiIteratif = 0;
-    int hasil = 1;
-    for (int i = 1; i <= n; i++) {
-        hasil *= i;
-        operasiIteratif++;     // hitung operasi
-    }
-    return hasil;
-}
-
-// Faktorial rekursif dengan penghitung
-int faktorialRekursifCount(int n) {
-    operasiRekursif++;
-    if (n <= 1) return 1;
-    return n * faktorialRekursifCount(n - 1);
-}
-
-void menu() {
+void menuUtama() {
     cout << "\n=== PROGRAM PELUANG ===\n";
-    cout << "1. Peluang Bernoulli\n";
-    cout << "2. Peluang Binomial\n";
-    cout << "3. Peluang Hipergeometrik\n";
-    cout << "4. Bandingkan Iteratif vs Rekursif\n";
+    cout << "1. Peluang Binomial\n";
     cout << "0. Keluar\n";
     cout << "Pilih menu: ";
+}
+
+void menuOperator() {
+    cout << "\n=== PILIH OPERASI ===\n";
+    cout << "1. P(X = x)\n";
+    cout << "2. P(X <= x)\n";
+    cout << "3. P(X >= x)\n";
+    cout << "4. P(X < x)\n";
+    cout << "5. P(X > x)\n";
+    cout << "6. P(a < X < b)\n";
+    cout << "7. P(a <= X <= b)\n";
+    cout << "Pilih: ";
 }
 
 int main() {
     int pilihan;
 
-    do {
-        menu();
+    while (true) {
+        menuUtama();
         cin >> pilihan;
 
-        if (pilihan == 1) {
-            int k;
-            float p;
-            cout << "k (0/1): "; cin >> k;
-            cout << "p: "; cin >> p;
-            cout << "Hasil = " << peluangBernoulli(k, p) << endl;
+        if (pilihan == 0) {
+            cout << "Program selesai.\n";
+            break;
         }
 
-        else if (pilihan == 2) {
+        switch (pilihan) {
+
+        case 1: {
             int n, k;
             float p;
-            cout << "n: "; cin >> n;
-            cout << "k: "; cin >> k;
-            cout << "p: "; cin >> p;
-            cout << "Hasil = " << peluangBinomial(n, k, p) << endl;
+            int op;
+
+            cout << "Masukkan n: "; cin >> n;
+            cout << "Masukkan p: "; cin >> p;
+
+            menuOperator();
+            cin >> op;
+
+            switch (op) {
+            case 1: // P(X = x)
+                cout << "Masukkan x: "; cin >> k;
+                cout << "Iteratif  = " << peluangBinomialIteratif(n, k, p) << endl;
+                cout << "Rekursif  = " << peluangBinomialRekursif(n, k, p) << endl;
+                break;
+
+            case 2: // P(X <= x)
+                cout << "Masukkan x: "; cin >> k;
+                {
+                    float total1 = 0, total2 = 0;
+                    for (int i = 0; i <= k; i++) {
+                        total1 += peluangBinomialIteratif(n, i, p);
+                        total2 += peluangBinomialRekursif(n, i, p);
+                    }
+                    cout << "Iteratif  = " << total1 << endl;
+                    cout << "Rekursif  = " << total2 << endl;
+                }
+                break;
+
+            case 3: // P(X >= x)
+                cout << "Masukkan x: "; cin >> k;
+                {
+                    float total1 = 0, total2 = 0;
+                    for (int i = k; i <= n; i++) {
+                        total1 += peluangBinomialIteratif(n, i, p);
+                        total2 += peluangBinomialRekursif(n, i, p);
+                    }
+                    cout << "Iteratif  = " << total1 << endl;
+                    cout << "Rekursif  = " << total2 << endl;
+                }
+                break;
+
+            case 4: // P(X < x)
+                cout << "Masukkan x: "; cin >> k;
+                {
+                    float total1 = 0, total2 = 0;
+                    for (int i = 0; i < k; i++) {
+                        total1 += peluangBinomialIteratif(n, i, p);
+                        total2 += peluangBinomialRekursif(n, i, p);
+                    }
+                    cout << "Iteratif  = " << total1 << endl;
+                    cout << "Rekursif  = " << total2 << endl;
+                }
+                break;
+
+            case 5: // P(X > x)
+                cout << "Masukkan x: "; cin >> k;
+                {
+                    float total1 = 0, total2 = 0;
+                    for (int i = k+1; i <= n; i++) {
+                        total1 += peluangBinomialIteratif(n, i, p);
+                        total2 += peluangBinomialRekursif(n, i, p);
+                    }
+                    cout << "Iteratif  = " << total1 << endl;
+                    cout << "Rekursif  = " << total2 << endl;
+                }
+                break;
+
+            case 6: { // P(a < X < b)
+                int a, b;
+                cout << "Masukkan a: "; cin >> a;
+                cout << "Masukkan b: "; cin >> b;
+
+                float total1 = 0, total2 = 0;
+                for (int i = a+1; i < b; i++) {
+                    total1 += peluangBinomialIteratif(n, i, p);
+                    total2 += peluangBinomialRekursif(n, i, p);
+                }
+
+                cout << "Iteratif  = " << total1 << endl;
+                cout << "Rekursif  = " << total2 << endl;
+                break;
+            }
+
+            case 7: { // P(a <= X <= b)
+                int a, b;
+                cout << "Masukkan a: "; cin >> a;
+                cout << "Masukkan b: "; cin >> b;
+
+                float total1 = 0, total2 = 0;
+                for (int i = a; i <= b; i++) {
+                    total1 += peluangBinomialIteratif(n, i, p);
+                    total2 += peluangBinomialRekursif(n, i, p);
+                }
+
+                cout << "Iteratif  = " << total1 << endl;
+                cout << "Rekursif  = " << total2 << endl;
+                break;
+            }
+
+            default:
+                cout << "Pilihan tidak valid.\n";
+            }
+            break;
         }
 
-        else if (pilihan == 3) {
-            int N, r, n, x;
-            cout << "N: "; cin >> N;
-            cout << "r: "; cin >> r;
-            cout << "n: "; cin >> n;
-            cout << "x: "; cin >> x;
-            cout << "Hasil = " << peluangHiperGeometrik(N, r, n, x) << endl;
+        default:
+            cout << "Pilihan tidak valid.\n";
         }
-
-        else if (pilihan == 4) {
-            int n;
-            cout << "Masukkan n (contoh 10/15/20): ";
-            cin >> n;
-
-            // Iteratif
-            faktorialIteratifCount(n);
-            long long iterOps = operasiIteratif;
-
-            // Rekursif
-            operasiRekursif = 0;
-            faktorialRekursifCount(n);
-            long long rekOps = operasiRekursif;
-
-            cout << "\n--- PERBANDINGAN ---\n";
-            cout << "Operasi Iteratif  : " << iterOps << endl;
-            cout << "Operasi Rekursif : " << rekOps << endl;
-
-            cout << "\nKesimpulan: ";
-            if (iterOps < rekOps)
-                cout << "Iteratif lebih efisien.\n";
-            else
-                cout << "Rekursif lebih banyak operasi.\n";
-        }
-
-        else if (pilihan == 0) {
-            cout << "Program selesai.\n";
-        }
-
-        else {
-            cout << "Pilihan tidak valid!\n";
-        }
-
-    } while (pilihan != 0);
+    }
 
     return 0;
 }
