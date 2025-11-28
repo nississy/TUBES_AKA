704f1c16781d2b4cd29a975f02f80b1c964c5ed0
Nisa <theunknownbusiness1010@gmail.com>
2025-11-28 16:04:05 +0700

    Delete kode_tubes_aka directory

--- kode_tubes_aka/tp.cpp
+++
@@ -1,3 +0,0 @@
-#include <iostream>
-#include "tp.h"
-using namespace std;
--- kode_tubes_aka/tp.h
+++
@@ -1,8 +0,0 @@
-#ifndef TP_H_INCLUDED
-#define TP_H_INCLUDED
-#include <iostream>
-using namespace std;
-
-
-
-#endif // TP_H_INCLUDED
