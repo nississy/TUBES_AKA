ca7d30865b0c17dd6b813b013cb2db296b89cf71
Nisa <theunknownbusiness1010@gmail.com>
2025-11-28 19:08:20 +0700

    Update peluang.cpp

--- TUBESAKA/peluang.cpp
+++ TUBESAKA/peluang.cpp
@@ -1,27 +1,20 @@
-#include <iostream>
+#include <cmath>
 #include "peluang.h"
-
-using namespace std;
-
-float pangkat(float x, int y) {
-    float hasil = 1.0;
-    for (int i = 0; i < y; i++) {
-        hasil = hasil  * x;
-    }
-    return hasil;
-}
 
 int faktorialIteratif(int n) {
     int hasil = 1;
-    for (int i = 1; i <= n; i++) hasil *= i;
+    for (int i = 1; i <= n; i++){
+        hasil *= i;
+    }
     return hasil;
 }
 
 int faktorialRekursif(int n) {
     if (n <= 1){
-      return 1;
+        return 1;
+    } else {
+        return n * faktorialRekursif(n - 1);
     }
-    return n * faktorialRekursif(n - 1);
 }
 
 long long kombinasiIteratif(int n, int k) {
@@ -31,28 +24,17 @@ long long kombinasiIteratif(int n, int k) {
 long long kombinasiRekursif(int n, int k) {
     if (k == 0 || k == n){
         return 1;
-    }
-    return kombinasiRekursif(n - 1, k - 1) + kombinasiRekursif(n - 1, k);
-}
-
-float peluangBernoulli(int k, float p) {
-    if (k == 1){
-        return p;
     } else {
-        return 1 - p;
+        return kombinasiRekursif(n - 1, k - 1) + kombinasiRekursif(n - 1, k);
     }
 }
 
-float peluangBinomial(int n, int k, float p) {
-    float C;
-    C = kombinasiIteratif(n, k);
-    return C * pangkat(p, k) * pangkat(1 - p, n - k);
+float peluangBinomialIteratif(int n, int k, float p) {
+    float C = kombinasiIteratif(n, k);
+    return C * pow(p, k) * pow(1 - p, n - k);
 }
 
-float peluangHiperGeometrik(int N, int r, int n, int x) {
-    int atas  = kombinasiIteratif(r, x) * kombinasiIteratif(N - r, n - x);
-    int bawah = kombinasiIteratif(N, n);
-    return (float)atas / (float)bawah;
+float peluangBinomialRekursif(int n, int k, float p) {
+    long long C = kombinasiRekursif(n, k);
+    return C * pow(p, k) * pow(1 - p, n - k);
 }
-
-
