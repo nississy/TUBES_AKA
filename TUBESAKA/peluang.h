c3ef174ebe8075349b594c696a77489c96fb0bf3
Nisa <theunknownbusiness1010@gmail.com>
2025-11-28 19:08:37 +0700

    Update peluang.h

--- TUBESAKA/peluang.h
+++ TUBESAKA/peluang.h
@@ -1,22 +1,15 @@
 #ifndef PELUANG_H_INCLUDED
 #define PELUANG_H_INCLUDED
-
-float pangkat(float x, int y);
-
-// Faktorial
+#include <iostream>
+using namespace std;
 
 int faktorialIteratif(int n);
 int faktorialRekursif(int n);
 
-// Kombinasi
-
 long long kombinasiIteratif(int n, int k);
 long long kombinasiRekursif(int n, int k);
 
-// Distribusi Khusus Peubah Acak Diskret
+float peluangBinomialIteratif(int n, int k, float p);
+float peluangBinomialRekursif(int n, int k, float p);
 
-float peluangBernoulli(int k, float p);
-float peluangBinomial(int n, int k, float p);
-float peluangHiperGeometrik(int N, int r, int n, int x);
-
-#endif // PELUANG_H_INCLUDED
+#endif
